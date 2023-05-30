8f26ffcc34ba46efdeecb3eca5b8f021f32b5d8e
Maxime Koler <maximekoler04@gmail.com>
2023-05-30 15:32:04 +0200

    Ajout de LabyDessin

---
+++ zeldiablo/src/gameLaby/laby/LabyDessin.java
@@ -0,0 +1,23 @@
+package gameLaby.laby;
+
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.paint.Color;
+import moteurJeu.DessinJeu;
+import moteurJeu.Jeu;
+
+public class LabyDessin implements DessinJeu {
+    @Override
+    public void dessinerJeu(Jeu jeu, Canvas canvas) {
+        LabyJeu laby = (LabyJeu) jeu;
+
+        // recupere un pinceau pour dessiner
+        final GraphicsContext gc = canvas.getGraphicsContext2D();
+        // dessin fond
+        gc.setFill(Color.LIGHTGRAY);
+        gc.fillRect(0, 0, canvas.getWidth(), canvas.getHeight());
+
+        gc.setFill(Color.BLACK);
+
+    }
+}
